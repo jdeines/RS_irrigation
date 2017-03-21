3d1ae8f77cb755561562809a570ea279b692e0a1
Jill Deines <jillian.deines@gmail.com>
2017-03-21 12:03:44 -0700

    updated vpoints to kml for generality

--- 1.155_Gee_vPoints_to_kml.R
+++ 1.155_Gee_vPoints_to_kml.R
@@ -9,19 +9,25 @@
 library(rgdal)
 
 filedir <- 'C:/Users/deinesji/Google Drive/GEE_validation/validationSets'
-fileName <- '2002_locations_NE_randAll_1050'
+fileName <- '2002_data_NE_randAll_1050_jmd_annual' # no ext
 
 outDir <- 'C:/Users/deinesji/Dropbox/1PhdJill/hpa/irrigation/data/GIS/validation/generatedPointsForUpload'
-outName <- '2002_locations_NE_randAll_1050.kml'
+outName <- '2002_data__NE_randAll_1050_jmd_annual1036.kml'
+
+latcolumn <- 'lat'
+longcolumn <- 'long'
+
+#columnsToKeep <- c('GI_max_14') # for location only datasets
+columnsToKeep <- c('class','certainty','classNum')
 
 # -------------------------------------------------------------------
 
 # load data
 vpoints <- read.csv(paste0(filedir, '/', fileName, '.csv'))
-vpoints <- vpoints[,c('GI_max_14','latitude','longitude')]
+vpoints <- vpoints[,c(columnsToKeep,latcolumn,longcolumn)]
 
 # spatialize
-coordinates(vpoints) <- ~ longitude + latitude
+coordinates(vpoints) <- cbind(longcolumn, latcolumn)
 proj4string(vpoints) <- CRS("+proj=longlat +datum=WGS84")
 
 # export
